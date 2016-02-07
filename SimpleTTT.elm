7dabdcb0c072226f741272b09b1894f896a72841
mark <mark@localhost.localdomain>
2016-02-07 13:10:26 +0100

    elm-formatted

--- SimpleTTT.elm
+++ SimpleTTT.elm
@@ -30,16 +30,16 @@ type GameState
 
 
 type alias Model =
-    { field : Field, drawingSize : Int, state : GameState, pos : Int,numFree:Int }
+    { field : Field, drawingSize : Int, state : GameState, pos : Int, numFree : Int }
 
 
 
 -- UPDATE
 
+
 type Mode
     = PvP
     | PvAI
-
 
 
 type UAction
@@ -55,32 +55,58 @@ type Action
 
 
 winningCombinations : List (List Int)
-winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
+winningCombinations =
+    [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 4, 8 ], [ 6, 4, 2 ] ]
+
 
 won : Player -> Field -> Bool
-won player field = List.any (List.all (\i -> case (get i field) of
-                                    Just (Pl pl) -> player == pl
-                                    _ -> False )) winningCombinations
+won player field =
+    List.any
+        (List.all
+            (\i ->
+                case (get i field) of
+                    Just (Pl pl) ->
+                        player == pl
+
+                    _ ->
+                        False
+            )
+        )
+        winningCombinations
+
 
 whowon : Int -> Field -> GameState
-whowon numFree field = case (won PlayerX field,won PlayerO field) of
-                (True,False) -> Won PlayerX
-                (False,True) -> Won PlayerO
-                (False,False) -> if numFree == 0 then Stalemate else Ongoing
-                _ -> Debug.crash "should not happen"
+whowon numFree field =
+    case ( won PlayerX field, won PlayerO field ) of
+        ( True, False ) ->
+            Won PlayerX
+
+        ( False, True ) ->
+            Won PlayerO
+
+        ( False, False ) ->
+            if numFree == 0 then
+                Stalemate
+            else
+                Ongoing
+
+        _ ->
+            Debug.crash "should not happen"
 
 
 update : Action -> Model -> Model
-update a ({field,numFree,drawingSize,pos} as model) =
+update a ({ field, numFree, drawingSize, pos } as model) =
     case a of
         SelectField x player ->
             case (get x field) of
-                Just Free -> let newField =  set x (Pl player) field
-                             in
-                             { model | field = newField , state = whowon (numFree-1) newField, numFree = numFree-1}
+                Just Free ->
+                    let
+                        newField = set x (Pl player) field
+                    in
+                        { model | field = newField, state = whowon (numFree - 1) newField, numFree = numFree - 1 }
 
-
-                _ ->  model
+                _ ->
+                    model
 
         Reset ->
             initialModel drawingSize pos
@@ -92,15 +118,25 @@ update a ({field,numFree,drawingSize,pos} as model) =
 
 view : Signal.Address UAction -> Bool -> Model -> Element
 view address highlight { field, drawingSize, state, pos } =
-  case state of
-    Ongoing ->
-        flow outward
-          [ if highlight then image drawingSize drawingSize "images/high_field.png" else image drawingSize drawingSize "images/field.png"
-          , flow down <| List.map (\i -> flow right <| toList <| indexedMap (\j c -> getImage address (drawingSize//3) ( pos, i * 3 + j ) c) (slice (i * 3) (i * 3 + 3) field)) [0..2]
-          ]
-    Won PlayerX -> image drawingSize drawingSize "images/cross.png"
-    Won PlayerO -> image drawingSize drawingSize "images/circle.png"
-    Stalemate -> image drawingSize drawingSize "images/stalemate.png"
+    case state of
+        Ongoing ->
+            flow
+                outward
+                [ if highlight then
+                    image drawingSize drawingSize "images/high_field.png"
+                  else
+                    image drawingSize drawingSize "images/field.png"
+                , flow down <| List.map (\i -> flow right <| toList <| indexedMap (\j c -> getImage address (drawingSize // 3) ( pos, i * 3 + j ) c) (slice (i * 3) (i * 3 + 3) field)) [0..2]
+                ]
+
+        Won PlayerX ->
+            image drawingSize drawingSize "images/cross.png"
+
+        Won PlayerO ->
+            image drawingSize drawingSize "images/circle.png"
+
+        Stalemate ->
+            image drawingSize drawingSize "images/stalemate.png"
 
 
 getImage : Signal.Address UAction -> Int -> ( Int, Int ) -> Content -> Element
@@ -119,4 +155,4 @@ getImage address drawsize pos c =
 
 initialModel : Int -> Int -> Model
 initialModel drawsize p =
-    { field = repeat 9 Free, drawingSize = drawsize, state = Ongoing, pos = p ,numFree = 9}
+    { field = repeat 9 Free, drawingSize = drawsize, state = Ongoing, pos = p, numFree = 9 }
--- UltimateTTT.elm
+++ UltimateTTT.elm
@@ -1,6 +1,6 @@
 module UltimateTTT (..) where
 
-import SimpleTTT exposing (Player,Mode)
+import SimpleTTT exposing (Player, Mode)
 import Array exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Input exposing (..)
@@ -11,6 +11,7 @@ import Color exposing (..)
 
 -- MODEL
 
+
 type GameState
     = Won Player
     | Stalemate
@@ -19,12 +20,13 @@ type GameState
 
 
 type alias Model =
-    { board : Array SimpleTTT.Model,
-     drawingSize : Int,
-     state : GameState,
-     mode : Mode,
-     currentPlayer : Player,
-     lastPlay : Maybe Int}
+    { board : Array SimpleTTT.Model
+    , drawingSize : Int
+    , state : GameState
+    , mode : Mode
+    , currentPlayer : Player
+    , lastPlay : Maybe Int
+    }
 
 
 
@@ -45,64 +47,101 @@ otherPlayer p =
             SimpleTTT.PlayerX
 
 
-
 won : Player -> Model -> Bool
-won player model = List.any (List.all (\i -> case (get i model.board) of
-                                    Just simpleModel -> case simpleModel.state of
-                                      SimpleTTT.Won pl-> pl == player
-                                      SimpleTTT.Ongoing -> False
-                                      SimpleTTT.Stalemate -> False
-                                    _ -> False )) SimpleTTT.winningCombinations
+won player model =
+    List.any
+        (List.all
+            (\i ->
+                case (get i model.board) of
+                    Just simpleModel ->
+                        case simpleModel.state of
+                            SimpleTTT.Won pl ->
+                                pl == player
+
+                            SimpleTTT.Ongoing ->
+                                False
+
+                            SimpleTTT.Stalemate ->
+                                False
+
+                    _ ->
+                        False
+            )
+        )
+        SimpleTTT.winningCombinations
 
 
 won2 : Player -> Model -> GameState
-won2  p model = if  Debug.watch " player won: " (won p model) then Won p
-                         else (if (List.any (\subfield -> subfield.state == SimpleTTT.Ongoing) (toList model.board)) then Ongoing else Stalemate)
-
+won2 p model =
+    if Debug.watch " player won: " (won p model) then
+        Won p
+    else
+        (if (List.any (\subfield -> subfield.state == SimpleTTT.Ongoing) (toList model.board)) then
+            Ongoing
+         else
+            Stalemate
+        )
 
 
 update : Action -> Model -> Model
-update a  ({board,drawingSize,state,mode,currentPlayer,lastPlay} as model) =
+update a ({ board, drawingSize, state, mode, currentPlayer, lastPlay } as model) =
     case a of
         SimpleTTT.Play ( x, y ) ->
             case (get x board) of
                 Just field ->
                     let
-
                         newField = (SimpleTTT.update (SimpleTTT.SelectField y currentPlayer) field)
-                        newBoard = set x newField  board
 
-                        newLastPlay = case (get y newBoard) of
-                          Just subfield -> case subfield.state of
-                            SimpleTTT.Ongoing -> Just y
-                            SimpleTTT.Stalemate -> Nothing
-                            SimpleTTT.Won _ -> Nothing
-                          _ -> Debug.crash "should not happen3"
+                        newBoard = set x newField board
 
-                        newModel = { model | board = newBoard, currentPlayer = otherPlayer currentPlayer, lastPlay = newLastPlay}
+                        newLastPlay =
+                            case (get y newBoard) of
+                                Just subfield ->
+                                    case subfield.state of
+                                        SimpleTTT.Ongoing ->
+                                            Just y
+
+                                        SimpleTTT.Stalemate ->
+                                            Nothing
+
+                                        SimpleTTT.Won _ ->
+                                            Nothing
+
+                                _ ->
+                                    Debug.crash "should not happen3"
+
+                        newModel = { model | board = newBoard, currentPlayer = otherPlayer currentPlayer, lastPlay = newLastPlay }
 
                         newState = won2 currentPlayer newModel
 
-                        valid = (case (get y field.field) of
-                                    Just (SimpleTTT.Free) -> True
-                                    _ -> False) &&
-                                 (field.state == SimpleTTT.Ongoing) &&
-                                 (Maybe.withDefault True (Maybe.map ((==)x) lastPlay)) &&
-                                 (state == Ongoing)
+                        valid =
+                            (case (get y field.field) of
+                                Just SimpleTTT.Free ->
+                                    True
 
-
+                                _ ->
+                                    False
+                            )
+                                && (field.state == SimpleTTT.Ongoing)
+                                && (Maybe.withDefault True (Maybe.map ((==) x) lastPlay))
+                                && (state == Ongoing)
                     in
-                      if Debug.watch "valid" valid then
-                          { newModel | state=newState}
-                      else model
+                        if Debug.watch "valid" valid then
+                            { newModel | state = newState }
+                        else
+                            model
 
                 Nothing ->
                     Debug.crash "cannot happen"
 
         SimpleTTT.UReset ->
             init drawingSize
-        SimpleTTT.SelectStart -> {model | state = Ongoing}
-        SimpleTTT.Select newmode -> {model | mode = newmode }
+
+        SimpleTTT.SelectStart ->
+            { model | state = Ongoing }
+
+        SimpleTTT.Select newmode ->
+            { model | mode = newmode }
 
 
 fieldClickedMailbox : Mailbox Action
@@ -110,43 +149,70 @@ fieldClickedMailbox =
     Signal.mailbox SimpleTTT.UReset
 
 
+
 -- VIEW
+
 
 init : Int -> Model
 init drawSize =
-    { board = (initialize 9 (SimpleTTT.initialModel (drawSize//3))), drawingSize = drawSize, state = NotStarted, mode = SimpleTTT.PvP, currentPlayer = SimpleTTT.PlayerX, lastPlay = Nothing }
+    { board = (initialize 9 (SimpleTTT.initialModel (drawSize // 3))), drawingSize = drawSize, state = NotStarted, mode = SimpleTTT.PvP, currentPlayer = SimpleTTT.PlayerX, lastPlay = Nothing }
 
-bigRedText: String -> Element
-bigRedText s = leftAligned <| Text.color red <| Text.height 80 <| fromString s
+
+bigRedText : String -> Element
+bigRedText s =
+    leftAligned <| Text.color red <| Text.height 80 <| fromString s
+
 
 view : Signal.Address SimpleTTT.UAction -> Model -> Element
-view address ({state, drawingSize} as model) =
-  case state of
-      Ongoing -> drawModel address model
-      Won p -> flow outward [drawModel address model,container drawingSize drawingSize middle <| bigRedText ((toString p)++ " won.")]
-      Stalemate -> flow outward [drawModel address model,container drawingSize drawingSize middle <| bigRedText "Stalemate"]
-      NotStarted -> startView address
+view address ({ state, drawingSize } as model) =
+    case state of
+        Ongoing ->
+            drawModel address model
+
+        Won p ->
+            flow outward [ drawModel address model, container drawingSize drawingSize middle <| bigRedText ((toString p) ++ " won.") ]
+
+        Stalemate ->
+            flow outward [ drawModel address model, container drawingSize drawingSize middle <| bigRedText "Stalemate" ]
+
+        NotStarted ->
+            startView address
 
 
-startView: Signal.Address SimpleTTT.UAction -> Element
-startView address = container 700 500 middle <|
-                    flow down [Graphics.Element.size 250 80 <| button (message address SimpleTTT.SelectStart) "Start", --clickable Graphics.Element.Element (message address SimpleTTT.SelectStart) <| centered <| Text.color red <| Text.height 120 (fromString "Start"),
-                       width 250 <| dropDown (\mode -> message address <| SimpleTTT.Select mode) [("Player vs. Player",SimpleTTT.PvP),("Player vs AI",SimpleTTT.PvAI)]
-                      ]
+startView : Signal.Address SimpleTTT.UAction -> Element
+startView address =
+    container 700 500 middle
+        <| flow
+            down
+            [ Graphics.Element.size 250 80 <| button (message address SimpleTTT.SelectStart) "Start"
+            , --clickable Graphics.Element.Element (message address SimpleTTT.SelectStart) <| centered <| Text.color red <| Text.height 120 (fromString "Start"),
+              width 250 <| dropDown (\mode -> message address <| SimpleTTT.Select mode) [ ( "Player vs. Player", SimpleTTT.PvP ), ( "Player vs AI", SimpleTTT.PvAI ) ]
+            ]
+
 
 drawModel : Signal.Address SimpleTTT.UAction -> Model -> Element
-drawModel address {drawingSize, board,lastPlay} = let highlighted =  case lastPlay of
-                                                  Nothing -> \_ _ -> True
-                                                  Just n -> \x y -> n == x*3+y
-                          in
+drawModel address { drawingSize, board, lastPlay } =
+    let
+        highlighted =
+            case lastPlay of
+                Nothing ->
+                    \_ _ -> True
 
-                          flow outward
-                                [ image drawingSize drawingSize "images/field.png"
-                                , flow down <| List.map (\i -> flow right <| toList <|
-                                   indexedMap (\j c -> SimpleTTT.view address (highlighted i j) c) (slice (i * 3) (i * 3 + 3) board)) [0..2]
-                                ]
-
-
+                Just n ->
+                    \x y -> n == x * 3 + y
+    in
+        flow
+            outward
+            [ image drawingSize drawingSize "images/field.png"
+            , flow down
+                <| List.map
+                    (\i ->
+                        flow right
+                            <| toList
+                            <| indexedMap (\j c -> SimpleTTT.view address (highlighted i j) c) (slice (i * 3) (i * 3 + 3) board)
+                    )
+                    [0..2]
+            ]
 
 
 main : Signal Element
